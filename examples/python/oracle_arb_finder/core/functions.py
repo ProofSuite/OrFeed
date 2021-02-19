6d0ff575a40848615450588939fa8f0338db125d
Eddine Omar <eddine.farid@hotmail.fr>
2021-02-19 13:39:18 +0100

    add comment and format code

--- examples/python/oracle_arb_finder/core/functions.py
+++ examples/python/oracle_arb_finder/core/functions.py
@@ -4,37 +4,78 @@ import pprint
 from core.token_infos import init_dict_token_dex
 from core.orfeed import Orfeed
 from dotenv import load_dotenv
+
 load_dotenv()
 import web3
 from web3 import Web3
 from tqdm import tqdm
 
-def getTokenToTokenPrice(orfeed_i, tokenSrc, tokenDst, provider, amount_wei=1):
-    res = orfeed_i.getExchangeRate(tokenSrc, tokenDst, provider, amount_wei)
+
+def getTokenToTokenPrice(orfeed_i, tokenSrc, tokenDst, dex, amount_src_token=1):
+    """Get the rate of swap tokenSrc to tokenDst in a given Dex
+
+    Args:
+        orfeed_i (OrFeed): The instance of OrFeed class
+        tokenSrc (Symbol): Symbol of src token
+        tokenDst (Symbol): Symbol of dst token
+        dex (str): The Dex where the rate is going to be requested
+        amount_src_token (int, optional): Amount of src token. Defaults to 1 src token unit.
+
+    Returns:
+        Dict: Return a dict containing all relevant infos about the request
+    """
+    res = orfeed_i.getExchangeRate(tokenSrc, tokenDst, dex, amount_src_token)
 
     return {
-        "tokenSrc" : tokenSrc,
-        "tokenDst" : tokenDst,
-        "tokenPair" : tokenSrc + '-' + tokenDst,
-        "provider" : provider,
-        "price" : res
+        "tokenSrc": tokenSrc,
+        "tokenDst": tokenDst,
+        "tokenPair": tokenSrc + "-" + tokenDst,
+        "provider": dex,
+        "price": res,
     }
 
-def simple_getTokenToTokenPrice(orfeed_i, src_token, src_token_infos, dst_token, dst_token_infos):
+
+def simple_getTokenToTokenPrice(
+    orfeed_i, src_token, src_token_infos, dst_token, dst_token_infos
+):
+    """For a pair of token, retrieve prices between Uniswap Dex and Kyber Dex.
+
+    Args:
+        orfeed_i (OrFeed): Instance of OrFeed
+        src_token (Symbol): : Symbol of src token
+        src_token_infos (Dict): Dict containing src token infos (number of decimals and address)
+        dst_token (Symbol): Symbol of dst_token
+        dst_token_infos (Symbol): Dict containing dst token infos (number of decimals and address)
+
+    Returns:
+        Dict: Dict containing all infos about buy/sell.
+    """
     result = {}
     providers_list = ["UNISWAPBYSYMBOLV2", "KYBERBYSYMBOLV1"]
     tmp_res = {}
     for provider in providers_list:
-        buy = getTokenToTokenPrice(orfeed_i, src_token, dst_token, provider, amount_wei=10**src_token_infos['decimals'])
-        sell = getTokenToTokenPrice(orfeed_i, dst_token, src_token, provider, amount_wei=buy["price"])
+        buy = getTokenToTokenPrice(
+            orfeed_i,
+            src_token,
+            dst_token,
+            provider,
+            amount_src_token=10 ** src_token_infos["decimals"],
+        )
+        sell = getTokenToTokenPrice(
+            orfeed_i, dst_token, src_token, provider, amount_src_token=buy["price"]
+        )
         tmp_res[provider] = {
-            "buy_price_wei": buy["price"]/(10**dst_token_infos['decimals']),
-            "sell_price_wei": sell["price"]*buy["price"]/(10**(dst_token_infos['decimals'] + src_token_infos['decimals']))
+            "buy_price_wei": buy["price"] / (10 ** dst_token_infos["decimals"]),
+            "sell_price_wei": sell["price"]
+            * buy["price"]
+            / (10 ** (dst_token_infos["decimals"] + src_token_infos["decimals"])),
         }
         if buy["price"] > 0 and sell["price"] > 0:
             tmp_res[provider] = {
-                "buy_price_wei": buy["price"]/(10**dst_token_infos['decimals']),
-                "sell_price_wei": sell["price"]*buy["price"]/(10**(dst_token_infos['decimals'] + src_token_infos['decimals']))
+                "buy_price_wei": buy["price"] / (10 ** dst_token_infos["decimals"]),
+                "sell_price_wei": sell["price"]
+                * buy["price"]
+                / (10 ** (dst_token_infos["decimals"] + src_token_infos["decimals"])),
             }
         else:
             return None
--- examples/python/oracle_arb_finder/core/orfeed.py
+++ examples/python/oracle_arb_finder/core/orfeed.py
@@ -1,25 +1,69 @@
 from web3 import Web3
 from core.smartcontracts import my_smartcontracts
 
+
 class Orfeed:
+    """OrFeed class
+    Initialise the blockchain provider
+    Implement some methods in order to call method in OrFeed smartcontract
+    https://etherscan.io/address/0x8316b082621cfedab95bf4a44a1d4b64a6ffc336
+    """
     def __init__(self, web3):
-        self.orfeed_address = Web3.toChecksumAddress(my_smartcontracts["orfeed"]["address"])
+        """Initialize the blockchain provider
+
+        Args:
+            web3 (string): url of blockchain provider, can be Infura or AlchemyAPI
+        """
+        self.orfeed_address = Web3.toChecksumAddress(
+            my_smartcontracts["orfeed"]["address"]
+        )
         self.w3 = web3
-        self.orfeed_contract = self.w3.eth.contract(address=self.orfeed_address, abi=my_smartcontracts["orfeed"]["abi"])
+        self.orfeed_contract = self.w3.eth.contract(
+            address=self.orfeed_address, abi=my_smartcontracts["orfeed"]["abi"]
+        )
 
     def getExchangeRate(self, _from, _to, _provider, _amount):
+        """getExchange rate between two ERC20 tokens on a given DEX
+
+        Args:
+            _from (str): symbol of ERC20 token. Source token.
+            _to (str): symbol of ERC20 token. Destination token.
+            _provider (str): the Dex where you want to swap your tokens.
+            _amount (str): amount of source token you want to exchange.
+
+        Returns:
+            str: The amount of destination you get.
+        """
         if _from == _to or _amount <= 0:
             return -1
         try:
-            return self.orfeed_contract.functions.getExchangeRate(_from, _to, _provider, _amount).call()
+            return self.orfeed_contract.functions.getExchangeRate(
+                _from, _to, _provider, _amount
+            ).call()
         except Exception:
             return -1
 
     def getTokenAddress(self, symbol):
+        """Return address of a ERC20 token.
+
+        Args:
+            symbol (str): ERC20 token.
+
+        Returns:
+            str: address of ERC20 token in the blockchain.
+        """
         try:
             return self.orfeed_contract.functions.getTokenAddress(symbol).call()
         except Exception:
             return -1
 
     def getTokenDecimalCount(self, address):
+        """Retrieve number of decimals of an ERC20 token.
+
+        Args:
+            address (str): address of ERC20 token.
+
+        Returns:
+            str: Number of decimals of the ERC20 token given in arg.
+        """
         return self.orfeed_contract.functions.getTokenDecimalCount(address).call()
